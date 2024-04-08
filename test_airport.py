db4dc51543e2a285363b4fd4952f04e7c52a1c1b
costelburuiana <costelburuiana87@gmail.com>
2024-04-08 16:52:03 +0300

    added decorators to tests

--- test_airport.py
+++ test_airport.py
@@ -9,6 +9,7 @@ ENDPOINT = "https://airportgap.com/api"
 
 headers = {}
 
+@allure.title("Test generate token with invalid credentials")
 @allure.description("Test generate token with invalid credentials")
 def test_generate_token_invalid_credentials():
     
@@ -19,6 +20,8 @@ def test_generate_token_invalid_credentials():
     )
     assert response.status_code == 401
 
+@allure.title("Test generate token with correct credentials")
+@allure.severity(allure.severity_level.CRITICAL)
 @allure.description("Test generate token")
 def test_generate_token():
     EMAIL = os.environ.get("EMAIL")
@@ -34,18 +37,21 @@ def test_generate_token():
     global headers
     headers = {"Authorization": f"Bearer {bearer_token}"}
 
-@allure.description("Test fetching all airports")
+@allure.title("Test fetching all airports")
+@allure.description("Test fetching all airports from API")
 def test_fetch_all_airports():
     response = requests.get(ENDPOINT + "/airports")
-    assert response.status_code == 200
+    assert response.status_code == 1200
     assert len(response.json()) > 0
 
-@allure.description("Test fetching an airport by ID")
+@allure.title("Test getting 'OTP' airport by ID")
+@allure.description("Test fetching the airport by string ID (OTP)")
 def test_gets_the_airport_by_ID():
     response = requests.get(ENDPOINT + "/airports/OTP")
     assert response.status_code == 200
     assert response.json()["data"]["id"] == "OTP"
 
+@allure.title("Test distance between two airports")
 @allure.description("Test distance between two airports")
 def test_distance_between_two_airports():
     from_airport = "OTP"
@@ -63,6 +69,9 @@ def test_distance_between_two_airports():
     distance = distance_data["attributes"]["kilometers"]
     assert distance > 0
 
+
+@allure.title("Test adding an airport to favourites")
+@allure.severity(allure.severity_level.CRITICAL)
 @allure.description("Test adding an airport to favourites")
 def test_add_and_delete_airport_to_favourites():
     try:
@@ -87,11 +96,13 @@ def test_add_and_delete_airport_to_favourites():
     )
     assert response.status_code == 204
 
+@allure.title("Test fetching all airports")
 @allure.description("Test fetching all airports")
 def test_returns_all_favourite_airports():
     response = requests.get(ENDPOINT + "/favorites", headers=headers)
     assert response.status_code == 200
 
+@allure.title("Test delete all favourites airports")
 @allure.description("Test delete all favourites airports")
 def test_detele_all_favourite_airports():
     response = requests.delete(ENDPOINT + "/favorites/clear_all", headers=headers)

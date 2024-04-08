5a50b9537ae3388936f0d401d190c819df0c0c99
costelburuiana <costelburuiana87@gmail.com>
2024-04-08 09:18:24 +0300

    allure report working on local

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# api_test_python
+# API testing with Python, Pytest and pytest-allure plugin to generate a report
+
+### In this folder, I created automation tests using https://airportgap.com/ API.
+
+All the tests are running in a virtual environment (pipenv). To run the tests in the same enviroment, please follow these steps:
+
+* `pip install pipenv` → to install Pipenv on your local machine
+* `pipenv install` → the command will look for a Pipenv file. If it doesn’t exist, it will create a new environment and activate it. After the above command is run, we’ll find two new files: Pipfile and Pipenv.lock
+* `pipenv shell` → to activate already created pipenv environment
+* `pipenv install -r requirements.txt` → install dependencies from requirements.txt
+
+Bellow are the commands to run the tests:
+
+1. `pytest --alluredir=report` → will generate the JSON files for each test case in the `report` folder that will be used to view the report.
+2. `allure serve allure-results` → report will be open in the default browser.
+ 
+The credentials that generated the `bearer token` are stored in the `.env` file on local machine.
+
---
+++ test_airport.py
@@ -0,0 +1,98 @@
+import allure
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ENDPOINT = "https://airportgap.com/api"
+
+
+headers = {}
+
+@allure.description("Test generate token with invalid credentials")
+def test_generate_token_invalid_credentials():
+    
+    email = "example@email.com"
+    password = "Password123"
+    response = requests.post(
+        ENDPOINT + "/tokens", data={"email": email, "password": password}
+    )
+    assert response.status_code == 401
+
+@allure.description("Test generate token")
+def test_generate_token():
+    EMAIL = os.environ.get("EMAIL")
+    PASSWORD = os.environ.get("PASSWORD")
+
+    response = requests.post(
+        ENDPOINT + "/tokens", data={"email": EMAIL, "password": PASSWORD}
+    )
+    assert response.status_code == 200
+
+    bearer_token = response.json()["token"]
+    assert bearer_token is not None
+    global headers
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+
+@allure.description("Test fetching all airports")
+def test_fetch_all_airports():
+    response = requests.get(ENDPOINT + "/airports")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
+
+@allure.description("Test fetching an airport by ID")
+def test_gets_the_airport_by_ID():
+    response = requests.get(ENDPOINT + "/airports/OTP")
+    assert response.status_code == 200
+    assert response.json()["data"]["id"] == "OTP"
+
+@allure.description("Test distance between two airports")
+def test_distance_between_two_airports():
+    from_airport = "OTP"
+    to_airport = "BBU"
+
+    response = requests.post(
+        ENDPOINT + "/airports/distance", data={"from": from_airport, "to": to_airport}
+    )
+    assert response.status_code == 200
+
+    data = response.json()
+    distance_data = data["data"]
+    assert distance_data
+
+    distance = distance_data["attributes"]["kilometers"]
+    assert distance > 0
+
+@allure.description("Test adding an airport to favourites")
+def test_add_and_delete_airport_to_favourites():
+    try:
+        response = requests.post(
+            ENDPOINT + "/favorites",
+            data={"airport_id": "OTP", "note": "Biggest airport in Romania"},
+            headers=headers,
+        )
+        assert response.status_code == 201
+    except:
+        # Airport already in favourites
+        assert response.status_code == 500
+
+    id = response.json()["data"]["id"]
+    response = requests.get(
+        ENDPOINT + f"/favorites/{id}", data={f"id": "{id}"}, headers=headers
+    )
+    assert response.status_code == 200
+
+    response = requests.delete(
+        ENDPOINT + f"/favorites/{id}", data={f"id": "{id}"}, headers=headers
+    )
+    assert response.status_code == 204
+
+@allure.description("Test fetching all airports")
+def test_returns_all_favourite_airports():
+    response = requests.get(ENDPOINT + "/favorites", headers=headers)
+    assert response.status_code == 200
+
+@allure.description("Test delete all favourites airports")
+def test_detele_all_favourite_airports():
+    response = requests.delete(ENDPOINT + "/favorites/clear_all", headers=headers)
+    assert response.status_code == 204
